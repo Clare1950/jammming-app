a7e17a0f22ad65d89311f2969f13b8ab75d378e8
Clare Parkes <clare.parkes@gmail.com>
2022-12-18 18:37:35 +0000

    First commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar"
+
+import './App.css'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { name: "Help", artist: "The Beatles", album: "Abbey Road", id: 1 },
+        { name: "name2", artist: "artist2", album: "album2", id: 2 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist"></div>
+          <SearchResults searchResults={this.state.searchResults} />
+          <Playlist />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
